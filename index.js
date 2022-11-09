6d987b082a19bafead955b11db7991f2acc07695
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-11-09 17:30:52 +0600

    added json file

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ const port = process.env.PORT || 5000 ;
 app.use(cors())
 app.use(express.json())
 
+const data = require ('./data/service.json')
 app.get('/', (req,res ) => {
     res.send('Travels Service Server running ')
 })
 
+app.get('/service', (req,res) => {
+    res.send(data)
+})
 // test in the git hub 
 
 app.listen(port, ()=> {
