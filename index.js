086f0e8b857418c018e8d23bb5f65f7e69286438
Md.Ashrafull Islam <ashrafull130@gmail.com>
2022-11-10 07:41:06 +0600

    added service api details by id

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ const port = process.env.PORT || 5000 ;
 app.use(cors())
 app.use(express.json())
 
-const data = require ('./data/service.json')
+const services = require ('./data/service.json')
 app.get('/', (req,res ) => {
     res.send('Travels Service Server running ')
 })
-
+// all services data 
 app.get('/service', (req,res) => {
-    res.send(data)
+    res.send(services)
 })
-// test in the git hub 
+// get service details by id 
+app.get('/service-details/:id', (req,res)=> {
+    const newId = req.params.id ;
+    const selectedService = services.find(service => service._id === newId )
+    console.log(selectedService)
+    res.send(selectedService)
+})
+ 
 
 app.listen(port, ()=> {
     console.log('Travels service running on port ', port )
